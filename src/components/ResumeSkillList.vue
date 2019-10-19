21015a81df5641d349ab77bea82ec5e53fc787fb
Phillip Horrex <pjhorrex@gmail.com>
2019-10-19 16:48:21 -0400

    Resume skill list component

---
+++ src/components/ResumeSkillList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="skill-list">
+    <h5>{{ skillName }}</h5>
+    <ul>
+      <li v-for="(skill, key) in skillList" :key="key">
+        {{ skill }}
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    skillName: {
+      type: String,
+      required: true
+    },
+    skillList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
