289eda1d2cc4dbaca1a1419f3612f51faf845793
Phillip Horrex <pjhorrex@gmail.com>
2019-10-27 00:36:33 -0400

    Footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="text-light text-center py-2">
     
-    <p class="mb-1">&copy; {{ currentYear }} <g-link to="/" exact>Phil Horrex</g-link>.</p>
+    <p class="mb-1">&copy; {{ currentYear }} <g-link to="/" exact>Phil Horrex</g-link>. All rights reserved.</p>
     <p>Made with <a href="https://gridsome.org/" target="_blank">Gridsome</a> and <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>. Icons by <a href="https://fontawesome.com/" target="_blank">Font Awesome</a>.</p>
   </footer>
 </template>
