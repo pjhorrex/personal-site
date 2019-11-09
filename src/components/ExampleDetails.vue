2f7ab21414b3777c2058a4df2db91e5e42fc6942
Phillip Horrex <pjhorrex@gmail.com>
2019-11-09 16:36:28 -0500

    Animate transtion in and out for example work desc

--- src/components/ExampleDetails.vue
+++ src/components/ExampleDetails.vue
@@ -1,15 +1,23 @@
 <template>
+    <transition 
+        name="expand"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @leave="leave"
+        appear
+    >
+    <div>
     <div class="example-details p-3">
         <button class="close text-light" type="button" @click="$emit('example-clicked', {id: ''})">
             <FontAwesomeIcon :icon="['fas', 'times-circle']" />
         </button>
-        <div class="container my-5" v-if="node != null">
+        <div class="container mt-4 mb-2" v-if="node != null">
           <h3 class="text-light mt-3">{{ node.alt }}</h3>
           <div class="row">
             <div class="col-12">
                 <ul class="framework-list list-inline">
                     <li class="list-inline-item">Built with:&nbsp;</li>
-                    <li class="list-inline-item" v-for="framework of node.frameworks" :key="framework.name">
+                    <li class="list-inline-item my-1" v-for="framework of node.frameworks" :key="framework.name">
                         <a :href="framework.url" class="badge badge-light" target="_blank">
                             <FontAwesomeIcon
                                 v-if="framework.fa != ''"
@@ -36,6 +44,8 @@
           </div>
         </div>
     </div>
+    </div>
+    </transition>
 </template>
 
 <script>
@@ -51,20 +61,52 @@ export default {
         node: {
             required: false
         }
+    },
+    methods: {
+        enter(element) {
+            element.style.height = 'auto';
+            const height = getComputedStyle(element).height;
+            element.style.height = 0;
+            // Force repaint
+            getComputedStyle(element).height;
+
+            this.$nextTick(() => element.style.height = height);
+        },
+        afterEnter(element) {
+            element.style.height = 'auto';
+        },
+        leave(element) {
+            const height = getComputedStyle(element).height;
+            element.style.height = height;
+            // Force repaint
+            getComputedStyle(element).height;
+
+            this.$nextTick(() => element.style.height = 0);
+        },
     }
 }
 </script>
 
-<style lang="scss">
-  .example-details {
-      background-color: $dark;
-      min-height: 200px;
-      color: $light;
-  }
+<style lang="scss" scoped>
+.example-details {
+    background-color: $dark;
+    color: $light;
+}
 
-  .framework-list {
-      a {
-          font-size: 1em;
-      }
-  }
+.framework-list {
+    a {
+        font-size: 1em;
+    }
+}
+
+.expand-enter-active,
+.expand-leave-active {
+  transition: height 0.5s ease-in-out;
+  overflow: hidden;
+}
+
+.expand-enter,
+.expand-leave-to {
+  height: 0;
+}
 </style>
